8479b7c9d49aa685ec624750d23e8cfb9e3e3caa
xiaohu <1209325963@qq.com>
2020-02-14 16:35:08 +0800

    第一次提交

---
+++ src/main/java/com/zhou/leetcode_anwser/LeetcodeAnwserApplication.java
@@ -0,0 +1,13 @@
+package com.zhou.leetcode_anwser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeAnwserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetcodeAnwserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zhou/leetcode_anwser/LeetcodeAnwserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhou.leetcode_anwser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeAnwserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
